90109c113e4c687eed0d0e85bde9b6dd6cfde14e
tyler quinn <tyler.quinn032@gmail.com>
2022-11-28 20:40:26 -0800

    format html, began UI logic
    Co-authored-by: Jason Markus <jasonlmarkus@gmail.com>
    Co-authored-by: Geoffrey Gao <geoffreygao1@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 function BankAccount() {
   this.name = " ";
   this.balance = 0;
@@ -6,5 +8,34 @@ function BankAccount() {
 
 
 BankAccount.prototype.deposit = function (depositAmount) {
-  this.balance += depositAmount;
+  this.balance += depositAmount.toFixed(2);
 }
+
+BankAccount.prototype.withdraw = function (withdrawAmount) {
+  this.balance -= withdrawAmount.toFixed(2);
+}
+
+BankAccount.prototype.updateName = function (nameOnAccount) {
+  this.name = nameOnAccount;
+}
+
+BankAccount.prototype.displayBalance = function () {
+  return "$" + this.balance;
+}
+
+// User Interface Logic
+
+// registerHandler => create a new bankAccount object from the form, unhide the current balance, and update current balance
+
+// moneyActionHandler => use our functionalities to modify the bankAccount, and update current balance
+
+// overdraft => do something if we withdraw more than we wanted to
+
+window.addEventListener("load",function() {
+let registerSection = querySelector("form#register-account");
+let depositButton = document.getElementById("depositButton");
+let withdrawButton = document.getElementById("withdrawButton");
+registerSection.addEventListener("submit", registerHandler);
+depositButton.addEventListener("click", depositHandler);
+withdrawButton.addEventListener("click", withdrawHandler);
+});
